5e0318b1f2956b3d763eb784aab397df5f43cc32
Luiz Chaves <luizchaves31@gmail.com>
2022-04-04 19:03:20 -0500

    Atualizacao

--- ubuntuserver/00-parametros.sh
+++ ubuntuserver/00-parametros.sh
@@ -56,7 +56,7 @@ USUARIODEFAULT="senac"
 SENHADEFAULT="123@senac"
 #
 # Variável do Nome (Hostname) do Servidor Ubuntu desse curso
-NOMESERVER="wschaves
+NOMESERVER="wschaves"
 #
 # Variável do Nome de Domínio do Servidor Ubuntu desse curso
 # OBSERVAÇÃO IMPORTANTE: essa variável será utilizada em outras variáveis desse curso
